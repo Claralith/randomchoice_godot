9ef339acd5d6fadc39fd7a1cedab51ccd21f63ea
Clara Grande <claragrande@proton.me>
2025-08-25 01:59:40 +0200

    add button and logic to delete items

--- control.gd
+++ control.gd
@@ -5,7 +5,9 @@ extends Control
 @onready var button = $Button
 @onready var random_choice_button = $RandomChoiceButton
 @onready var selected_random_choice_text = $RandomChoiceText/SelectedRandomChoiceText
+@onready var delete_item_button = $DeleteItemButton
 var focus
+var selected_list_item
 
 func add_item():
 	var text = item_to_add.text.strip_edges()
@@ -13,23 +15,26 @@ func add_item():
 		items.add_item(text)
 		item_to_add.text = ""
 
+func delete_item():
+	print(items.get_selected_items())
+	for item in items.get_selected_items():
+		items.remove_item(item)
+
 var rng = RandomNumberGenerator.new()
 func get_random_choice():
 	if items.item_count >= 2:
 		var randomNumber = rng.randi_range(0, items.item_count-1)
 		selected_random_choice_text.text = items.get_item_text(randomNumber)
-		print(randomNumber)
-		print(selected_random_choice_text.text)
-	#if items.item_count == 1:
-		##selected_random_choice_text = items.get_children()[0]
-		#print(items.get_index(1))
+	if items.item_count == 1:
+		selected_random_choice_text.text = items.get_item_text(0)
 
 func _input(event):
 	if event.is_action_pressed("AddItem"):
 		if focus == item_to_add:
 			add_item()
-			#print(items)
 			item_to_add.grab_focus()
+	#if event.is_action_pressed("Delete"):
+		#items.remove_item(selected_list_item)
 
 func _on_focus_changed(control: Control) -> void:
 	focus = control
@@ -39,3 +44,5 @@ func _ready() -> void:
 	button.pressed.connect(add_item)
 	random_choice_button.pressed.connect(get_random_choice)
 	get_viewport().gui_focus_changed.connect(_on_focus_changed)
+	delete_item_button.pressed.connect(delete_item)
+	
